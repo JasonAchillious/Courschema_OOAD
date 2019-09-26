a5705de15e92b6d782cee4e6a9bf038dc10d9ca1
于志洋 <50703949+rgseyvie@users.noreply.github.com>
2019-09-26 20:27:58 +0800

    Add files via upload

---
+++ Backend/src/main/Major.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class Mahor{
+    private int idMajor;
+    private String Name;
+    private int Department;
+
+    public int getIdMajor() {
+        return idMajor;
+    }
+
+    public void setIdMajor(int idMajor) {
+        this.idMajor = idMajor;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(int Department) {
+        this.Department = Department;
+    }
+
+    public Major(int idMajor, String Name, int Department){
+        this.idMajor = idMajor;
+        this.Name = Name;
+        this.Department = Department;
+    }
+
+}
