03264d373d8350daeea2e09ecbac79c06f736c43
于志洋 <50703949+rgseyvie@users.noreply.github.com>
2019-12-03 17:58:47 +0800

    Add files via upload

---
+++ Backend/springproject/src/main/java/com/exercise/springproject/service/CourseServiceImpl.java
@@ -0,0 +1,34 @@
+package com.exercise.springproject.service;
+
+import com.exercise.springproject.api.CourseRepository;
+import com.exercise.springproject.domain.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseServiceImpl implements CourseService{
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Override
+    public List<Course> findAll() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Course save(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Course findCourseById(int idCourse){
+        return courseRepository.findCourseById(idCourse);
+    }
+
+    @Override
+    public void deleteCourseById(int idCourse) {
+        courseRepository.deleteCourseById(idCourse);
+    }
+}
