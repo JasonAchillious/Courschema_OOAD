7f4c84bc2e287fcc5f015bdcadba117c0f5e6706
于志洋 <50703949+rgseyvie@users.noreply.github.com>
2019-12-03 18:01:58 +0800

    Add files via upload

---
+++ Backend/springproject/src/main/java/com/exercise/springproject/web/CourseController.java
@@ -0,0 +1,63 @@
+package com.exercise.springproject.web;
+
+import com.exercise.springproject.domain.Course;
+import com.exercise.springproject.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping("/recordCourse")
+    public List<Course> findAllCourse(){
+        return courseService.findAll();
+    }
+
+    @PostMapping("/recordClassification")
+    public Course addOne(Course course){
+        return courseService.save(course);
+    }
+
+    @PutMapping("/recordClassification")
+    public Course update(@RequestParam int idCourse,
+                                 @RequestParam int courschema,
+                                 @RequestParam String code,
+                                 @RequestParam String chineseName,
+                         @RequestParam String intro,
+                         @RequestParam double credit,
+                         @RequestParam byte spring,
+                         @RequestParam byte autumn,
+                         @RequestParam byte summer,
+                         @RequestParam String englishName,
+                         @RequestParam int year){
+        Course course = new Course();
+        course.setIdCourse(idCourse);
+        course.setAutumn(autumn);
+        course.setChineseName(chineseName);
+        course.setCode(code);
+        course.setCredit(credit);
+        course.setEnglishName(englishName);
+        course.setIntro(intro);
+        course.setSpring(spring);
+        course.setSummer(summer);
+        course.setYear(year);
+
+        return courseService.save(course);
+    }
+
+    @PostMapping("findCourse")
+    public Course findCourseById(@RequestParam int idCourse){
+        return courseService.findCourseById(idCourse);
+    }
+
+    @PostMapping("findCourseCname")
+    public String findCourseCnameById(@RequestParam int idCourse){
+        return courseService.findCourseById(idCourse).getChineseName();
+    }
+
+}
