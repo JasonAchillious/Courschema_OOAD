70dfeb04fb0db5f7e941a31837d17abecbb8d9ac
evanna <evannazhang@outlook.com>
2019-12-04 10:32:29 +0800

    lalala

--- Backend/springproject/src/main/java/com/exercise/springproject/api/StudentRepository.java
+++ Backend/springproject/src/main/java/com/exercise/springproject/api/StudentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<student, Integer> {
     public student findStudentByName(String name);
-    @Query("select p from student p where p.id=?1")
+    @Query("select p from student p where p.id_student=?1")
     public student findStudentByid_student(int id);
 
     //public Student findUserByNameAndPassword(String name, String password);
