6a59e8569fc46ec6dec834d6f741c1ef8b5c4284
evanna <evannazhang@outlook.com>
2019-12-04 10:45:55 +0800

    lalala

--- Backend/springproject/src/main/java/com/exercise/springproject/api/MajorRepository.java
+++ Backend/springproject/src/main/java/com/exercise/springproject/api/MajorRepository.java
@@ -1,7 +1,6 @@
 package com.exercise.springproject.api;
 
 
-import com.exercise.springproject.domain.Course;
 import com.exercise.springproject.domain.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
