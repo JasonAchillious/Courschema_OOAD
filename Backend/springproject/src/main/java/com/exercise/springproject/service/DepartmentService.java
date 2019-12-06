4f065f47ff71d74935bab7a1573888784d57cd2b
于志洋 <50703949+rgseyvie@users.noreply.github.com>
2019-12-06 17:38:14 +0800

    Delete DepartmentService.java

--- Backend/springproject/src/main/java/com/exercise/springproject/service/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.springproject.service;
-
-import com.exercise.springproject.domain.Department;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface DepartmentService {
-    public Department findDepartmentById(int idDepartment);
-}
