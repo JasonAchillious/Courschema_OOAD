3f2eaa13b4be94a0f9a66b2fd825112ca68a9403
evanna <evannazhang@outlook.com>
2019-12-08 19:07:26 +0800

    lalala

---
+++ Backend/springproject/src/main/java/com/exercise/springproject/domain/SchemaEdit.java
@@ -0,0 +1,42 @@
+package com.exercise.springproject.domain;
+
+import java.util.List;
+
+public class SchemaEdit {
+    private List<Integer> tongshi;
+    private List<Integer> ruxi;
+    private List<Integer> bixiu;
+    private List<Integer> xuanxiu;
+
+    public List<Integer> getBixiu() {
+        return bixiu;
+    }
+
+    public List<Integer> getRuxi() {
+        return ruxi;
+    }
+
+    public List<Integer> getXuanxiu() {
+        return xuanxiu;
+    }
+
+    public List<Integer> getTongshi() {
+        return tongshi;
+    }
+
+    public void setBixiu(List<Integer> bixiu) {
+        this.bixiu = bixiu;
+    }
+
+    public void setRuxi(List<Integer> ruxi) {
+        this.ruxi = ruxi;
+    }
+
+    public void setTongshi(List<Integer> tongshi) {
+        this.tongshi = tongshi;
+    }
+
+    public void setXuanxiu(List<Integer> xuanxiu) {
+        this.xuanxiu = xuanxiu;
+    }
+}
