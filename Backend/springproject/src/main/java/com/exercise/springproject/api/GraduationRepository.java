0d9329070af4fbfb95935031e54eddbddbe7fbac
evanna <evannazhang@outlook.com>
2019-12-10 11:53:52 +0800

    lalala

---
+++ Backend/springproject/src/main/java/com/exercise/springproject/api/GraduationRepository.java
@@ -0,0 +1,18 @@
+package com.exercise.springproject.api;
+
+import com.exercise.springproject.domain.graduate_condition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface GraduationRepository extends JpaRepository<graduate_condition, Integer> {
+    @Query("select p from graduate_condition p where p.idCourse=?1")
+    public List<graduate_condition> findgraduate_conditionByIdCourse(int idCourse);
+
+    @Query("select p from graduate_condition p where p.courschema=?1")
+    public List<graduate_condition> findgraduate_conditionByCourschema(int idschema);
+
+    @Query("select p from graduate_condition p where p.idCourse=?1 and p.courschema=?2")
+    public graduate_condition findgraduate_conditionByIdCourseAndCourschema(int idCourse, int idschema);
+}
