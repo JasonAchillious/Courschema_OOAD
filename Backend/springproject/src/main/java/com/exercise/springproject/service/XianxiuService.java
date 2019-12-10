7ac25971307370a75099a912f131808de7156f4b
evanna <evannazhang@outlook.com>
2019-12-10 16:20:36 +0800

    lalala

--- Backend/springproject/src/main/java/com/exercise/springproject/service/XianxiuService.java
+++ Backend/springproject/src/main/java/com/exercise/springproject/service/XianxiuService.java
@@ -1,7 +1,14 @@
 package com.exercise.springproject.service;
 
+import com.exercise.springproject.domain.Xianxiu_condition;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface XianxiuService {
+    public List<Xianxiu_condition> findXianxiu_conditionByIdCourse(int idCourse);
+    public List<Xianxiu_condition> findXianxiu_conditionByCourschema(int schema);
+    public Xianxiu_condition findXianxiu_conditionByIdCourseAndCourschema(int course, int schema);
 }
--- Backend/springproject/src/main/java/com/exercise/springproject/service/XianxiuServiceImpl.java
+++ Backend/springproject/src/main/java/com/exercise/springproject/service/XianxiuServiceImpl.java
@@ -1,11 +1,29 @@
 package com.exercise.springproject.service;
 
 import com.exercise.springproject.api.XianXiuRepository;
+import com.exercise.springproject.domain.Xianxiu_condition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class XianxiuServiceImpl implements XianxiuService{
     @Autowired
     private XianXiuRepository xianXiuRepository;
+
+    @Override
+    public List<Xianxiu_condition> findXianxiu_conditionByIdCourse(int idCourse) {
+        return xianXiuRepository.findXianxiu_conditionByIdCourse(idCourse);
+    }
+
+    @Override
+    public List<Xianxiu_condition> findXianxiu_conditionByCourschema(int schema) {
+        return xianXiuRepository.findXianxiu_conditionByCourschema(schema);
+    }
+
+    @Override
+    public Xianxiu_condition findXianxiu_conditionByIdCourseAndCourschema(int course, int schema) {
+        return xianXiuRepository.findXianxiu_conditionByIdCourseAndCourschema(course, schema);
+    }
 }
